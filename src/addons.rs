49bfbb51c438472e63f6686d0b25fea88b61f4f5
ProbablySophie <70588905+probablySophie@users.noreply.github.com>
2024-09-19 20:13:31 +0800

    addons

---
+++ src/addons.rs
@@ -0,0 +1,17 @@
+
+use crate::todo::Todo;
+
+pub fn on_completion(todo: Todo) -> Todo
+{
+    // TODO: 
+
+    todo // return todo
+}
+
+pub fn on_creation(todo: Todo) -> Todo
+{
+    // TODO:
+
+    todo // return todo
+}
+
