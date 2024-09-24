c869b77f246da0f96e1b9909028f56b2a6407d15
ProbablySophie <70588905+probablySophie@users.noreply.github.com>
2024-09-24 17:35:02 +0800

    a completed todo item, and tests to prove it

--- src/test.rs
+++ src/test.rs
@@ -1,13 +1,38 @@
 #[cfg(test)]
+use crate::Todo;
+#[cfg(test)]
+use crate::shared::date::Date;
+
 
 #[test]
 fn empty_todo()
 {
-    use crate::Todo;
-
     let todo = Todo::from("");
     
     assert_eq!(todo.to_string(), "");
+}
+
+#[test]
+fn full_todo()
+{
+	let from_str: &str = "x (R) 2024-03-14 2000-01-01 Test description +project @context tag:onetwo";
+	
+	let todo = Todo::from(from_str);
+
+	assert!(todo.complete);
+	assert!(todo.priority == Some('R'));
+	
+	assert!(todo.completion_date.is_some_and(|d| d == Date::from("2024-03-14").unwrap()));
+	assert!(todo.creation_date.is_some_and(|d| d == Date::from("2000-01-01").unwrap()));
+
+	assert!(todo.tags.matches_project("project"));
+	assert!(todo.tags.matches_context("context"));
+	
+	assert!(todo.tags.has_tag("tag"));
+	assert_eq!(todo.tags.tag_value("tag"), Ok("onetwo".to_string()));
+
+	assert_eq!(todo.to_string(), from_str);
+	
 }
 
 // TODO: more testing of the Todo struct
