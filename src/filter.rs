80ed1af0d0545f33bb9c819b067b285a86056f4d
ProbablySophie <70588905+probablySophie@users.noreply.github.com>
2024-09-24 10:06:50 +0800

    THE TESTS PASS!!!

--- src/filter.rs
+++ src/filter.rs
@@ -28,6 +28,8 @@ pub fn creation_date(todo_vec: &Vec<Todo>, creation_date: Date) -> Vec<Todo>
 	filter_vec!(todo_vec, {|todo| todo.creation_date == Some(creation_date) })
 }
 
+// TODO: Filter by completion date
+
 pub fn project(todo_vec: &Vec<Todo>, project: &str) -> Vec<Todo>
 {
 	filter_vec!(todo_vec, {|todo| todo.tags.matches_project(project) })
@@ -61,19 +63,78 @@ pub fn incomplete(todo_vec: &Vec<Todo>) -> Vec<Todo>
 mod test
 {
 	use super::*;
+	use crate::Todo;
+	use crate::shared::date::Date;
 
-	fn make_todo_vec() //-> Vec<Todo>
+	fn make_todo_vec() -> Vec<Todo>
 	{
+		vec![
+			// Priority.  Creation Date.
+			Todo::from("x 2016-05-20 2016-04-30 description description +project1 @context1"),
+			Todo::from("x (A) 2016-05-24 2016-04-19 description description"),
+			// Due Date.  Priority
+			Todo::from("(C) description description +project2 @context2 @context3 due:1969-07-20"),
+			// Creation Date.  
+			Todo::from("description +project3 @context3"),
+		]
 		// TODO: Make a todo vec
+	}
+
+	#[test]
+	fn test_priority()
+	{
+		let todo_vec = make_todo_vec();
+		assert!(priority(&todo_vec, 'C').len() == 1);
+		assert!(priority(&todo_vec, 'B').is_empty());
+	}
+	#[test]
+	fn filter_due_date()
+	{
+		let todo_vec = make_todo_vec();
+		assert!(due_date(&todo_vec, Date::from("1969-07-20").unwrap()).len() == 1);
+	}
+	#[test]
+	fn filter_creation_date()
+	{
+		let todo_vec = make_todo_vec();
+		assert!(creation_date(&todo_vec, Date::from("2016-04-30").unwrap()).len() == 1);
+	}
+	#[test]
+	fn filter_project()
+	{
+		let todo_vec = make_todo_vec();
+		assert!(project(&todo_vec, "project1").len() == 1);
+		assert!(project(&todo_vec, "project2").len() == 1);
+		assert!(project(&todo_vec, "project3").len() == 1);
+		assert!(project(&todo_vec, "project4").is_empty());
+	}
+	#[test]
+	fn filter_context()
+	{
+		let todo_vec = make_todo_vec();
+		assert!(context(&todo_vec, "context1").len() == 1);
+		assert!(context(&todo_vec, "context2").len() == 1);
+		assert!(context(&todo_vec, "context3").len() == 2);
+		assert!(context(&todo_vec, "context4").is_empty());
+	}
+	#[test]
+	fn filter_has_tag()
+	{
+		let todo_vec = make_todo_vec();
+		assert!(has_tag(&todo_vec, "due").len() == 1);
+		assert!(has_tag(&todo_vec, "fake").is_empty());
+	}
+	#[test]
+	fn filter_complete()
+	{
+		let todo_vec = make_todo_vec();
+		assert!(complete(&todo_vec).len() == 2);
+	}
+	#[test]
+	fn filter_incomplete()
+	{
+		let todo_vec = make_todo_vec();
+		assert!(incomplete(&todo_vec).len() == 2);
 	}
 }
 
-// TODO: Testing for the filters
-//       * Priority
-//       * due_date
-//       * creation_date
-//       * project
-//       * context
-//       * has_tag
-//       * complete
-//       * incomplete
