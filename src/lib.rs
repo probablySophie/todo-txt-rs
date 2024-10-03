d07a1605f1157eae1f87a16395566df6f0e78691
ProbablySophie <70588905+probablySophie@users.noreply.github.com>
2024-10-03 22:31:17 +0800

    crate info & documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+//! # TODO-txt
+//! 
+//! TODO-txt is a rust crate that follows the [todo.txt](https://github.com/todotxt/todo.txt) standard for creating and saving TODO items.
+//! This crate has some additional functionality on top of the base standard aimed at making implementing the crate into an app a far simpler process, including filtering, sorting, and more.
+
 #![allow(dead_code)]
 
 mod shared;
@@ -6,16 +11,19 @@ mod todo;
 pub use todo::*;
 mod tags;
 
-//mod sort;
+mod info;
+mod sort;
 mod filter;
 //mod addons;
 
-#[path = "tests/main.rs"] mod test;
+#[path = "tests/lib_tests.rs"] mod test;
 
 /*
    The Todo.txt documentation: https://github.com/todotxt/todo.txt
 */
 #[must_use] // the caller of this function MUST USE the returned result
+/// Makes a `Vec<Todo>` from a given &str.
+/// The input &str is assumed to be multi-line and will be split as such
 pub fn bulk_create(string: &str) -> Vec<todo::Todo>
 {
     let mut todo_vec = Vec::new();
@@ -43,6 +51,8 @@ pub fn bulk_create(string: &str) -> Vec<todo::Todo>
 
 
 #[must_use]
+/// Creates a `String` from a given `Vec<Todo>`
+/// This function is intended to be used for exporting & saving between active sessions
 pub fn flatten_vec(todo_vec: Vec<todo::Todo>) -> String
 {
     let mut string = String::new();
@@ -51,6 +61,8 @@ pub fn flatten_vec(todo_vec: Vec<todo::Todo>) -> String
     {
         string += &(todo.to_string() + "\n");
     }
-
+    // Remove the last newline :)
+    string.pop();
+    
     string // return string
 }
